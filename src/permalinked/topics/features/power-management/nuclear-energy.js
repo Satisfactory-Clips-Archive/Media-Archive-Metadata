48d9735b341849c18364c7a6cc09b5662830a0fd
SignpostMarv <me@signpostmarv.name>
2021-01-21 03:02:54 +0000

    deploying util function for deduplication

--- src/permalinked/topics/features/power-management/nuclear-energy.js
+++ src/permalinked/topics/features/power-management/nuclear-energy.js
@@ -1,4 +1,5 @@
 const satisfactory = require('../../../../common/coffee-stain.js');
+const {SatisfactoryWikiImage} = require('../../../../utils');
 
 module.exports = [
 	{
@@ -7,50 +8,30 @@ module.exports = [
 		"name": "Nuclear Energy",
 		"description": "Satisfactory Livestream clips about Nuclear Energy",
 		"image": [
+			SatisfactoryWikiImage(
+				{
+					wikiname: 'Nuclear_Power_Plant.png',
+					encodingFormat: 'image/png',
+					width: 512,
+					height: 512,
+				},
 			{
-				"@type": "ImageObject",
 				"name": "In-game building icon for the Nuclear Power Plant.",
 				"contentUrl": "https://static.wikia.nocookie.net/satisfactory_gamepedia_en/images/4/46/Nuclear_Power_Plant.png/revision/latest?cb=20200311145339&format=original",
-				"encodingFormat": "image/png",
-				"width": {
-					"@type": "QuantitativeValue",
-					"value": 512
+				}
+			),
+			SatisfactoryWikiImage(
+				{
+					wikiname: 'Nuclear_Waste.png',
+					encodingFormat: 'image/png',
+					width: 256,
+					height: 256,
 				},
-				"height": {
-					"@type": "QuantitativeValue",
-					"value": 512
-				},
-				"usageInfo": [
-					"https://satisfactory.gamepedia.com/Template:Copyright_first-party",
-					"https://www.fandom.com/licensing"
-				],
-				"discussionUrl": [
-					"https://satisfactory.gamepedia.com/File_talk:Nuclear_Power_Plant.png"
-				],
-				"url": "https://satisfactory.gamepedia.com/File:Nuclear_Power_Plant.png"
-			},
 			{
-				"@type": "ImageObject",
 				"name": "In-game item icon for Nuclear Waste.",
 				"contentUrl": "https://static.wikia.nocookie.net/satisfactory_gamepedia_en/images/a/a1/Nuclear_Waste.png/revision/latest?cb=20190626163606&format=original",
-				"encodingFormat": "image/png",
-				"width": {
-					"@type": "QuantitativeValue",
-					"value": 256
-				},
-				"height": {
-					"@type": "QuantitativeValue",
-					"value": 256
-				},
-				"usageInfo": [
-					"https://satisfactory.gamepedia.com/Template:Copyright_first-party",
-					"https://www.fandom.com/licensing"
-				],
-				"discussionUrl": [
-					"https://satisfactory.gamepedia.com/File_talk:Nuclear_Waste.png"
-				],
-				"url": "https://satisfactory.gamepedia.com/File:Nuclear_Waste.png"
 			}
+			),
 		],
 		"about": [
 			satisfactory,

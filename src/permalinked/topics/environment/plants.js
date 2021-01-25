87b81228e3255218943c96d0e803c36bc4c393e7
SignpostMarv <me@signpostmarv.name>
2021-01-25 11:21:50 +0000

    adding plants, expanding schema to allow related links

---
+++ src/permalinked/topics/environment/plants.js
@@ -0,0 +1,10 @@
+import {WebPage} from '../../../utils.js';
+
+export default [
+	WebPage('Plants', {
+		description: 'Satisfactory Livestream clips about Plants',
+		relatedLink: [
+			'https://satisfactory.gamepedia.com/Category:Flora',
+		],
+	}),
+];

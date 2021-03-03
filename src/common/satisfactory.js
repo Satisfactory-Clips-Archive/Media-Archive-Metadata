d8343f20939ef197ec12ef293b47e8efc27ff637
SignpostMarv <me@signpostmarv.name>
2021-03-03 00:39:36 +0000

    set url

--- src/common/satisfactory.js
+++ src/common/satisfactory.js
@@ -3,6 +3,7 @@ import {default as coffee_stain} from './coffee-stain.js';
 export default {
 	"@type": "VideoGame",
 	"name": "Satisfactory",
+	'url': 'https://satisfactorygame.com/',
 	"author": coffee_stain,
 	"operatingSystem": "Windows",
 	"applicationCategory": [

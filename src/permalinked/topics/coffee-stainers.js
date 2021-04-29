2750a40245873fcba3b644391213d2b6017246d8
SignpostMarv <me@signpostmarv.name>
2021-04-29 19:16:32 +0100

    adding additional metadata

---
+++ src/permalinked/topics/coffee-stainers.js
@@ -0,0 +1,18 @@
+import {
+	default as coffee_stain
+} from '../../common/coffee-stain.js';
+import {
+	WebPage,
+} from '../../utils.js';
+
+export default [
+	WebPage('Coffee Stainers', {
+		description: 'Satisfactory clips about past & present employees of Coffee Stain',
+		about: [
+			coffee_stain,
+		],
+		url: [
+			'https://satisfactory.fandom.com/wiki/Coffee_Stain_Studios',
+		],
+	}),
+];

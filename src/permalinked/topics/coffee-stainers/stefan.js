fa36a4a768ebd5f31d34716eac668a2206f98ba7
SignpostMarv <me@signpostmarv.name>
2021-06-24 23:15:33 +0100

    adding Stefan

---
+++ src/permalinked/topics/coffee-stainers/stefan.js
@@ -0,0 +1,15 @@
+import {
+	CoffeeStainer,
+} from '../../../utils.js';
+
+export default [
+	CoffeeStainer('Stefan Hanna', {
+		jobTitle: [
+			'CEO',
+			'Co-founder'
+		],
+		url: [
+			'https://twitter.com/stefan_hanna',
+		],
+	}),
+];

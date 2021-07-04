a9c3aed0ff569365b032340513aa90770b6f3bd6
SignpostMarv <me@signpostmarv.name>
2021-07-04 20:54:03 +0100

    adding manta & bird

---
+++ src/permalinked/topics/environment/creatures/giant-flying-manta.js
@@ -0,0 +1,21 @@
+import {SatisfactoryWikiImage, WebPage} from '../../../../utils.js';
+
+export default [
+	WebPage('Giant Flying Manta', {
+		description: 'Satisfactory Livestream clips about the Giant Flying Manta',
+		relatedLink: [
+			'https://satisfactory.fandom.com/Giant_Flying_Manta',
+		],
+		image: [
+			SatisfactoryWikiImage(
+				{
+					wikiname: 'Giant Flying Manta.png',
+					width: 1200,
+					height: 1200,
+					encodingFormat: 'image/png',
+					contentUrl: 'https://static.wikia.nocookie.net/satisfactory_gamepedia_en/images/5/54/Giant_Flying_Manta.png/revision/latest?cb=20210202003934',
+				}
+			),
+		],
+	}),
+];
---
+++ src/permalinked/topics/environment/creatures/non-flying-bird.js
@@ -0,0 +1,21 @@
+import {SatisfactoryWikiImage, WebPage} from '../../../../utils.js';
+
+export default [
+	WebPage('Non Flying Bird', {
+		description: 'Satisfactory Livestream clips about the Non Flying Bird',
+		relatedLink: [
+			'https://satisfactory.fandom.com/Non_Flying_Bird',
+		],
+		image: [
+			SatisfactoryWikiImage(
+				{
+					wikiname: 'Non Flying Bird.png',
+					width: 1200,
+					height: 1200,
+					encodingFormat: 'image/png',
+					contentUrl: 'https://static.wikia.nocookie.net/satisfactory_gamepedia_en/images/4/45/Non_Flying_Bird.png/revision/latest?cb=20210202075728',
+				}
+			),
+		],
+	}),
+];

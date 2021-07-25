63e3ff5aacea6b3d2ae8e4e8d47c0552ab2a7413
SignpostMarv <me@signpostmarv.name>
2021-07-25 21:46:06 +0100

    adding youtube video

--- src/permalinked/topics/coffee-stainers/tobias.js
+++ src/permalinked/topics/coffee-stainers/tobias.js
@@ -1,9 +1,17 @@
 import {
+	YouTubeVideo,
 	CoffeeStainer,
 } from '../../../utils.js';
 
 export default [
 	CoffeeStainer('Tobias Carlsson', {
 		jobTitle: 'Programmer',
+		subjectOf: [
+			YouTubeVideo('FyOHDcxtEUA', {
+				name: 'Dev Vlog: How drones work in Satisfactory (w/ old WIP footage)',
+				description: 'Hang out with Tobias and Jace as they discuss how drones work, how they were made, as well as showing off some old Work-In-Progress shots from early development.',
+				uploadDate: '2021-07-23',
+			}),
+		],
 	}),
 ];

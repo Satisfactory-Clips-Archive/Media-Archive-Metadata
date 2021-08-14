4e9c045eb59669f93850d8dcef9bc48ac2552d77
SignpostMarv <me@signpostmarv.name>
2021-08-14 11:17:34 +0100

    adding structured data for golf

---
+++ src/permalinked/topics/features/requested-features/golf.js
@@ -0,0 +1,30 @@
+import {
+	WebPage,
+	ImageObject,
+	YouTubePlaylist,
+} from '../../../../utils.js';
+
+export default [
+	WebPage('Golf', {
+		description: 'Satisfactory Livestream clips relating to the Golf feature request created during the April 27th, 2021 Livestream',
+		subjectOf: [
+			YouTubePlaylist('PLbjDnnBIxiEqUEBB3kU_ZkkEp6YBvweep', {
+				name: 'Golf',
+				startDae: '2021-04-27',
+			}),
+		],
+		relatedLink: [
+			'https://questions.satisfactorygame.com/post/60882899aa0ba107e325b301',
+		],
+		image: [
+			ImageObject(
+				{
+					contentUrl: 'https://i.img.archive.satisfactory.video/content/topics/features/requested-features/golf--bg.webp',
+					width: 336,
+					height: 504,
+					encodingFormat: 'image/webp',
+				}
+			),
+		]
+	}),
+];

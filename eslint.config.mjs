4ca3d8568d721e5d03ddce9940885dfdba0701a0
SignpostMarv <me@signpostmarv.name>
2026-01-30 11:56:54 +0000

    adding eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import {
+	typescript,
+} from '@signpostmarv/eslint-config';
+
+import parser from '@typescript-eslint/parser';
+
+// eslint-disable-next-line imports/no-default-export
+export default  [
+	{
+		languageOptions: {
+			parser,
+			parserOptions: {
+				project: ['./tsconfig.json'],
+			},
+		},
+	},
+	...typescript,
+	{
+		files: ['**/*.ts'],
+		ignores: ['**/*.d.ts', '**/*.js', '**/*.mjs'],
+	},
+];

2a2fae8c05072a734923d1880f27d6847ce723c2
SignpostMarv <me@signpostmarv.name>
2026-01-31 00:59:35 +0000

    resolving issue with CSS obj having no explicit type

--- src/common/coffee-stain.ts
+++ src/common/coffee-stain.ts
@@ -1,4 +1,9 @@
-const CSS = {
+import type {
+	Schema,
+	SchemaProperties,
+} from '../SchemaTypes.ts';
+
+const CSS: Schema.Organization<SchemaProperties.Organization> = {
 	'@type': 'Organization',
 	name: 'Coffee Stain Studios AB',
 	url: 'https://www.coffeestainstudios.com/',
